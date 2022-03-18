e3b9d5f8f4245b6650ad2017b1c6637923a03570
Leif Ashley <leifashley@gmail.com>
2022-03-18 13:30:00 -0400

    Initial Commit

---
+++ SwiftKORE/SwiftKORE.h
@@ -0,0 +1,18 @@
+//
+//  SwiftKORE.h
+//  SwiftKORE
+//
+//  Created by Leif Ashley on 3/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftKORE.
+FOUNDATION_EXPORT double SwiftKOREVersionNumber;
+
+//! Project version string for SwiftKORE.
+FOUNDATION_EXPORT const unsigned char SwiftKOREVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKORE/PublicHeader.h>
+
+
